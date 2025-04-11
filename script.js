75bdd71547f6cc1b54939e3a4b8bc41cef7922ca
livialimaetec <liviaalmeidaetec@gmail.com>
2025-04-11 13:51:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function addToCart(productId) {
+    // Implemente a lógica para adicionar o produto ao carrinho de compras
+    // Aqui você pode usar JavaScript para interagir com o backend ou fazer ações específicas ao adicionar um produto ao carrinho
+    console.log(`Produto adicionado ao carrinho: ${productId}`);
+  }
